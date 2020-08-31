9054661cad0ef4169c27fc19f07bf32850bb7f74
juanlrcricci <juanjo.ricci@live.com.ar>
2020-08-31 19:23:11 -0300

    trabajo7

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # trabajo7
+
+[![Build Status](https://travis-ci.org/juanlrcricci/trabajo7.svg?branch=master)](https://travis-ci.org/juanlrcricci/trabajo7)
+
+[![Coverage Status](https://coveralls.io/repos/github/juanlrcricci/trabajo7/badge.svg?branch=master)](https://coveralls.io/github/juanlrcricci/trabajo7?branch=master)
---
+++ productoServices.py
@@ -0,0 +1,75 @@
+from repositorios import Repositorios
+
+
+class ProductoService:
+
+    # Devuelve repositorio productoList
+    def get_productosList(self):
+        return Repositorios.productosList
+
+    # parametros : Object producto
+    # return: Key id
+    def add_producto(self, producto):
+        lastKey = -1
+        for key in Repositorios.productosList:
+            lastKey = key
+        id_new = int(lastKey) + 1
+        Repositorios.productosList[id_new] = producto.__dict__
+        return id_new
+
+    # parametros: key id, Object producto
+    def delete_producto(self, id):
+        if id not in Repositorios.productosList:
+            raise ValueError("El id a elminar no existe")
+        del Repositorios.productosList[id]
+
+    # Actualiza producto en repositorio memberList
+    # parametros :  key legajo , Object member
+    def update_producto(self, legajo, producto):
+        if id not in Repositorios.productoList:
+            raise ValueError("El legajo no existe")
+        Repositorios.productoList.update({legajo: producto.__dict__})
+
+    def insertion_sort_precio(self, lista, tipo_orden):
+        lista_ordenada = lista.copy()
+        for i in range(1, len(lista_ordenada)):
+            actual = lista_ordenada[i]
+            j = i
+            # Desplazamiento de los elementos
+            if tipo_orden == 'ascendente':
+                while j > 0 and \
+                 lista_ordenada[j-1]["_precio"] > actual["_precio"]:
+                    lista_ordenada[j] = lista_ordenada[j-1]
+                    j = j-1
+            if tipo_orden == 'descendente':
+                while j > 0 and \
+                 lista_ordenada[j-1]["_precio"] < actual["_precio"]:
+                    lista_ordenada[j] = lista_ordenada[j-1]
+                    j = j-1
+            # insertar el elemento en su lugar
+            lista_ordenada[j] = actual
+        return lista_ordenada
+
+    def busqueda_binaria(self, lista, precio_buscado):
+        lista_ordenada = lista.copy()
+        for i in range(1, len(lista_ordenada)):
+            actual = lista_ordenada[i]
+            j = i
+            while j > 0 and lista_ordenada[j-1]["_precio"] > actual["_precio"]:
+                lista_ordenada[j] = lista_ordenada[j-1]
+                j = j-1
+            # insertar el elemento en su lugar
+            lista_ordenada[j] = actual
+        izq = 0
+        der = len(lista_ordenada) - 1
+        while izq <= der:
+            medio = (izq+der)//2
+            pmed = lista_ordenada[medio]['_precio']
+            if pmed == precio_buscado:
+                # print (lista_ordenada[medio])
+                return lista_ordenada[medio]
+            elif pmed > precio_buscado:
+                der = medio-1
+            else:
+                izq = medio+1
+        return -1
